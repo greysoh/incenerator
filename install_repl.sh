eab006439762144d0df68a373860195479cc0abb
greysoh <greysoh@pm.me>
2022-08-19 16:48:35 -0400

    fix: Removes typo.

--- install_repl.sh
+++ install_repl.sh
@@ -9,7 +9,7 @@ echo "sed -i 's/bash local32.sh/node index.js/g' .replit" >> local32.sh
 echo "rm -rf local32.sh && node index.js" >> local32.sh
 echo "Modifing initialization files..."
 sed -i 's/cowsay Configure me!/bash local32.sh/g' .replit
-sed -i 's/pkgs.cowsay/pkgs.nodejs-16_x/g' replit.nix\
+sed -i 's/pkgs.cowsay/pkgs.nodejs-16_x/g' replit.nix
 echo "Adding proxy config..."
 cp example_conf.json config.json
 rm -rf example_conf.json
